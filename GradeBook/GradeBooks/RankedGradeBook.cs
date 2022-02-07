97335f5e29f983214706f6d37e014e12b103c308
Vitali Lungu <vitali.lungu@cegeka.com>
2022-02-07 23:12:07 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,5 @@
 ﻿using GradeBook.Enums;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +8,25 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade >= Students.Count * 20)
+                return 'A';
+            else if (averageGrade >= Students.Count * 40 && averageGrade <= Students.Count * 20)
+                return 'B';
+            else if (averageGrade >= 60 && averageGrade <= Students.Count * 40)
+                return 'C';
+            else if (averageGrade >= 80 && averageGrade <= Students.Count * 60)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
