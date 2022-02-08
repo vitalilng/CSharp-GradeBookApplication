7a2b638e60d4f198fd9e3cd7d7b09a558988dde0
Vitali Lungu <vitali.lungu@cegeka.com>
2022-02-08 15:25:30 +0200

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,7 +41,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];            
             var type = parts[2];
-            bool isWeighted = parts[3];
+            bool isWeighted = bool.Parse(parts[3]);
 
             BaseGradeBook gradeBook;
             if (type == "standard")
