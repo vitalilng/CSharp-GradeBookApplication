d9f0fe0a6c593e96965c73febdc82143660418fe
Vitali Lungu <vitali.lungu@cegeka.com>
2022-02-08 14:44:04 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,5 +35,27 @@ namespace GradeBook.GradeBooks
             else 
                 return 'F';            
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStudentStatistics(name);
+            }            
+        }
     }
 }
